d35907ceea7298bf77f5a656fb3e0a19fb829c38
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2022-07-28 18:08:12 +0200

    added new content

---
+++ src/_data/content.js
@@ -0,0 +1,23 @@
+module.exports = {
+    de: [
+      {
+        label: "Startseite",
+        url: "/de/"
+      },
+      {
+        label: "Blog",
+        url: "/de/blog/"
+      }
+    ],
+    en: [
+      {
+        label: "Home",
+        url: "/en/"
+      },
+      {
+        label: "Blog",
+        url: "/en/blog/"
+      }
+    ]
+  };
+  
