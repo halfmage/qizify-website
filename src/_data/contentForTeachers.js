443ef75ef07690e614cf44440f2b6f6879add0c5
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2022-07-31 17:57:05 +0200

    content-teachers, features-software and styles, etc.

--- src/_data/contentForTeachers.js
+++ src/_data/contentForTeachers.js
@@ -1,15 +1,15 @@
 module.exports = {
   de: {
-    title: "Nutzen Sie die Möglichkeiten von Qizify ",
-    text: "Wir haben gezielt nach den essenziellen Funktionen gesucht, die Studenten und Schüler brauchen, um effektiv zu lernen. Dabei nutzen wir eine optimale Mischung von Leistung, Spieltrieb und Wettbewerb",
+    title: "Als Lehrbeauftragter können Sie Lerninhalte einfach kontrollieren und verbessern",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda est quibusdam aut adipisci corrupti, inventore veniam!",
     listitem1: "Erstellen Sie Quizze basierend auf ihren Lerninhalten und teilen Sie sie mit Gruppenkollegen.",
     listitem2: "Erhalten Sie Karma-Punkte, wenn ihr Quiz von einem Dozenten bestätigt oder von anderen Lernenden gemocht wird, und nehmen Sie an der Rangliste teil.",
     listitem3: "Spielen Sie Quizzes, die von anderen Lernenden erstellt wurden.",
     listitem4: "Überprüfung, Analyse und Verbesserung der eigenen Lernleistung.",
   },
   en: {
-    title: "Optimized features for learners",
-    text: "We specifically looked for the essential functions that students need to learn effectively. We use an optimal mixture of performance, playfulness and competition",
+    title: "As a lecturer, you can easily check and improve learning content",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda est quibusdam aut adipisci corrupti, inventore veniam!",
     listitem1: "Create, review and verify quizzes.",
     listitem2: "Send feedback to a quiz author to improve it.",
     listitem3: "Check learners' performance per quiz",
---
+++ src/_data/featuresSoftware.js
@@ -0,0 +1,36 @@
+module.exports = {
+    de: [
+        {
+            label: "Database Thing",
+            text: "Wir schützen personenbezogene Daten aller Benutzer und Organisationen nach aktuellen und DSGVO-konformen Datenschutzrichtlinien.",
+            icon: "M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4"
+        },
+        {
+            label: "Customization Thing",
+            text: "Die Datenübertragung zwischen Ihrem Client und einer Datenbankinstanz wird durch SSL/TLS-Verbindungen verschlüsselt, wodurch eine zusätzliche Schutzebene hinzugefügt wird.",
+            icon: "M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"
+        },
+        {
+            label: "Language Thing",
+            text: "Unsere komplette Server- und Dateninfrastruktur befindet sich innerhalb Deutschland und wird ebenfalls in dort betreut und gepflegt.",
+            icon: "M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        }
+        ],
+        en: [
+        {
+            label: "Database Thing",
+            text: "We protect personal data of all users and organizations according to current and GDPR-compliant data protection guidelines.",
+            icon: "M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4"
+        },
+        {
+            label: "Customization Thing",
+            text: "Data transfer between your client and a database instance is encrypted by SSL/TLS connections, adding an extra layer of protection.",
+            icon: "M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"
+        },
+        {
+            label: "Language Thing",
+            text: "Our complete server and data infrastructure is located in Germany and is also looked after and maintained there.",
+            icon: "M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        }
+        ]
+};
