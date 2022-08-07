31fc01f6a7c479a5c2f20224dc0d3e0b569fac36
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2022-08-07 13:31:02 +0200

    added testimonials

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,11 +2,13 @@ module.exports = {
   de: {
     title: "Qizify",
     featuresSecureTitle: "Funktionen für die Sicherheit",
-    latestBlogPosts: "Letzte Blog Artikel"
+    latestBlogPosts: "Letzte Blog Artikel",
+    testimonials: "Was andere über uns sagen ..."
   },
   en: {
     title: "My website",
     featuresSecureTitle: "Features for security",
-    latestBlogPosts: "Latest blog posts"
+    latestBlogPosts: "Latest blog posts",
+    testimonials: "What others say about us ..."
   }
 }
---
+++ src/_data/testimonials.js
@@ -0,0 +1,36 @@
+module.exports = {
+    de: [
+        {
+            image: "testimonial-01.jpg",
+            quote: "Ich finde es sehr gut, dass Lehrende mir direktes Feedback über die Plattform geben können. So kann ich sicher sein, dass ich auf dem richtigen Lernweg bin.",
+            name: "NAME_TEXT",
+        },
+        {
+            image: "testimonial-02.jpg",
+            quote: "Endlich weiß ich, was ich lernen muss, um bei der Prüfung erfolgreich zu sein.",
+            name: "NAME_TEXT",
+        },
+        {
+            image: "testimonial-03.jpg",
+            quote: "Qizify Learning verbindet mich mit meinen Kommilitonen und bringt uns unserem Lernziel näher.",
+            name: "NAME_TEXT",
+        }
+    ],
+    en: [
+        {
+            image: "testimonial-01.jpg",
+            quote: "Ich finde es sehr gut, dass Lehrende mir direktes Feedback über die Plattform geben können. So kann ich sicher sein, dass ich auf dem richtigen Lernweg bin.",
+            name: "NAME_TEXT",
+        },
+        {
+            image: "testimonial-02.jpg",
+            quote: "Endlich weiß ich, was ich lernen muss, um bei der Prüfung erfolgreich zu sein.",
+            name: "NAME_TEXT",
+        },
+        {
+            image: "testimonial-03.jpg",
+            quote: "Qizify Learning verbindet mich mit meinen Kommilitonen und bringt uns unserem Lernziel näher.",
+            name: "NAME_TEXT",
+        }
+    ]
+};
