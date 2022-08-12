9537ec6e7d4d79edc465b01115f6507850ec2769
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2022-08-12 17:52:48 +0200

    nav and header fixes

--- src/_data/mainnav.js
+++ src/_data/mainnav.js
@@ -2,22 +2,56 @@ module.exports = {
   de: [
     {
       label: "Startseite",
-      url: "/de/"
+      url: "/de/",
+      slug: "home"
     },
     {
-      label: "Blog",
+      label: "Funktionen",
+      url: "/de/funktionen",
+      slug: "features"
+    },
+    {
+      label: "Preise",
+      url: "/de/preise",
+      slug: "pricing"
+    },
+    {
+      label: "Team",
+      url: "/de/team",
+      slug: "team"
+    },
+    {
+      label: "FAQ",
       url: "/de/blog/",
+      slug: "blog",
       blogPostLabel: "Alle Blogeinträge"
     }
   ],
   en: [
     {
       label: "Home",
-      url: "/en/"
+      url: "/en/",
+      slug: "home"
     },
     {
-      label: "Blog",
+      label: "Features",
+      url: "/en/features",
+      slug: "features"
+    },
+    {
+      label: "Pricing",
+      url: "/en/pricing",
+      slug: "pricing"
+    },
+    {
+      label: "Team",
+      url: "/en/team",
+      slug: "team"
+    },
+    {
+      label: "FAQ",
       url: "/en/blog/",
+      slug: "blog",
       blogPostLabel: "All blogpost"
     }
   ]
