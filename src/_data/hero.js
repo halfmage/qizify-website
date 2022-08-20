6d9c4ce8f7651cbea2b6b6c07c27cd4f4ff26c7e
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2022-08-20 17:23:56 +0200

    hero updates

--- src/_data/hero.js
+++ src/_data/hero.js
@@ -1,6 +1,6 @@
 module.exports = {
     de: {
-      title: "Unterrichte mit Qizify Learning.",
+      title: "Die digitale Lern-Platform für Universitäten",
       subtitle: "Überall & jederzeit",
       text: "Eine Online-Plattform für Lernende und Lehrende, die die Vorbereitung auf Prüfungen mithilfe von studiengangbezogenen Quizzes erleichtert.",
       screen1: "Quizze",
@@ -13,7 +13,7 @@ module.exports = {
       textTeam: "Das Team, das uns erfolgreich gemacht hat"
     },
     en: {
-      title: "Teach with Qizify Learning.",
+      title: "The digital learning platform for universities",
       subtitle: "Anytime & anywhere",
       text: "An online platform for learners and lecturers that facilitates self-preparation for exams with the help of quizzes related to a study program.",
       screen1: "Quizzes",
