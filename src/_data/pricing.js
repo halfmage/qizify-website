51d5c60407bcb3b07ea513c8fded3d2568876cdf
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2022-08-31 10:46:25 +0200

    pricing page updates

---
+++ src/_data/pricing.js
@@ -0,0 +1,34 @@
+module.exports = {
+    de: {
+        title: "Wir glauben an ein simples Bezahlsystem",
+        text: "Der Zugang zu Bildung und Lernmethoden sollte so einfach wie möglich gestaltet werden. Daher haben wir uns bei Qizify entschieden, ein einfaches, auf benötigten Umfang, Preismodell zu verwenden. Dadurch können, neben der vereinfachten Kalkulation von Kosten, ebenfalls Transparenz und Änderungswünsche gewährleistet werden.",
+        textListItem1: "Alle Features für eine eigenständige Universitäts-Lernplattform.",
+        textListItem2: "100% DSGVO-konform und durch SSL-Verbindungen abgesichert.",
+        textListItem3: "Überprüfung, Analyse und Verbesserung der eigenen Lernleistung.",
+        perUser: "pro Nutzer",
+        monthly: "Monatliche Gebühren",
+        adjustments: "Anpassungen möglich",
+        boxListItem1: "Inklusive Onboarding von Mitarbeitern, Software-Updates und Support.",
+        boxListItem2: "Die Vertragsdauer beträgt mindestens 6 Monate.",
+        boxListItem3: "Keine automatische Vertragsverlängerung.",
+        boxListItem4: "Preisnachlass für große Einrichtungen & Live-Demo auf Anfrage.",
+        buttonLabel: "Jetzt kontaktieren"
+
+    },
+    en: {
+        title: "We believe in a simple payment system",
+        text: "Access to education and learning methods should be made as easy as possible. Therefore, at Qizify, we decided to use a simple, on-demand pricing model. In addition to the simplified calculation of costs, transparency and change requests can also be guaranteed.",
+        textListItem1: "All features for an independent university learning platform.",
+        textListItem2: "100% GDPR compliant and secured by SSL connections.",
+        textListItem3: "Review, analysis and improvement of your own learning performance.",
+        perUser: "per User",
+        monthly: "Monthly Fees",
+        adjustments: "adjustments possible",
+        boxListItem1: "Includes employee onboarding, software updates and support.",
+        boxListItem2: "The contract period is at least 6 months.",
+        boxListItem3: "No automatic contract extension.",
+        boxListItem4: "Price discount for large facilities & live demo on request.",
+        buttonLabel: "Contact now"
+
+    }
+};
