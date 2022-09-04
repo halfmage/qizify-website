10bd6778ee3d29934604f96f3541e4250ec4bda4
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2022-09-04 17:53:58 +0200

    📝 contact form and footer

---
+++ src/_data/footernav.js
@@ -0,0 +1,36 @@
+module.exports = {
+de: [
+    {
+    label: "Impressum",
+    url: "/de/imprint",
+    slug: "imprint"
+    },
+    {
+    label: "Datenschutz",
+    url: "/de/privacy",
+    slug: "privacy"
+    },
+    {
+    label: "Nutzungsbedingungen",
+    url: "/de/terms-of-use",
+    slug: "terms-of-use"
+    }
+],
+en: [
+    {
+    label: "Imprint",
+    url: "/en/imprint",
+    slug: "imprint"
+    },
+    {
+    label: "Privacy Notice",
+    url: "/en/privacy",
+    slug: "privacy"
+    },
+    {
+    label: "Terms",
+    url: "/en/terms-of-use",
+    slug: "terms-of-use"
+    }
+]
+};
--- src/_data/site.js
+++ src/_data/site.js
@@ -5,7 +5,15 @@ module.exports = {
     featuresSoftwareTitle: "Funktionen die uns ausmachen",
     latestBlogPosts: "Letzte Blog Artikel",
     testimonials: "Was andere über uns sagen ...",
-    requestDemo: "Demo anfordern"
+    contact: "Kontakt",
+    requestDemo: "Demo anfordern",
+    contactName: "Dein Name",
+    contactMail: "Deine E-Mail-Adresse",
+    contactMessage: "Deine Nachricht",
+    contactSend: "Absenden",
+    contactCalendly: "Buchen Sie einen Demo-Termin",
+    contactCalendlySubline: "Ganz bequem einen freien Online-Termin auf Calendly auswählen.",
+    contactBox: "Ich stimme der Datenschutzerklärung zu und stimme zu, von Qizify per E-Mail kontaktiert zu werden. Ich stimme der Speicherung meiner übermittelten Daten für den erforderlichen Zeitraum zu."
   },
   en: {
     title: "Qizify Learning",
@@ -13,6 +21,14 @@ module.exports = {
     featuresSoftwareTitle: "Functions that define us",
     latestBlogPosts: "Latest blog posts",
     testimonials: "What others say about us ...",
-    requestDemo: "Request demo"
+    contact: "Contact",
+    requestDemo: "Request demo",
+    contactName: "Your name",
+    contactMail: "Your email",
+    contactMessage: "Your message",
+    contactSend: "Send",
+    contactCalendly: "Book a demo appointment",
+    contactCalendlySubline: "Easily select a free online appointment on Calendly.",
+    contactBox: "I agree to the privacy policy and I consent to being contacted by Qizify via email. I agree to the storage of my transmitted data for the required period of time."
   }
 }
