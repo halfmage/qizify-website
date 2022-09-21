bae40aee849b82d04eac5fc698f58cd56a38a53c
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2022-09-21 21:18:39 +0200

    more content updates and fixes

--- src/_data/site.js
+++ src/_data/site.js
@@ -6,13 +6,13 @@ module.exports = {
     latestBlogPosts: "Letzte Blog Artikel",
     testimonials: "Was andere über uns sagen ...",
     contact: "Kontakt",
-    requestDemo: "Demo anfordern",
+    requestDemo: "Demo & Kontakt",
     contactName: "Dein Name",
     contactMail: "Deine E-Mail-Adresse",
     contactMessage: "Deine Nachricht",
     contactSend: "Absenden",
-    contactCalendly: "Buchen Sie einen Demo-Termin",
-    contactCalendlySubline: "Ganz bequem einen freien Online-Termin auf Calendly auswählen.",
+    contactKoalendar: "Buchen Sie einen Demo-Termin",
+    contactKoalendarSubline: "Ganz bequem einen freien Online-Termin auf Koalendar auswählen.",
     contactBox: "Ich stimme der Datenschutzerklärung zu und stimme zu, von Qizify per E-Mail kontaktiert zu werden. Ich stimme der Speicherung meiner übermittelten Daten für den erforderlichen Zeitraum zu."
   },
   en: {
@@ -22,13 +22,13 @@ module.exports = {
     latestBlogPosts: "Latest blog posts",
     testimonials: "What others say about us ...",
     contact: "Contact",
-    requestDemo: "Request demo",
+    requestDemo: "Demo & contact",
     contactName: "Your name",
     contactMail: "Your email",
     contactMessage: "Your message",
     contactSend: "Send",
-    contactCalendly: "Book a demo appointment",
-    contactCalendlySubline: "Easily select a free online appointment on Calendly.",
+    contactKoalendar: "Book a demo appointment",
+    contactKoalendarSubline: "Easily select a free online appointment on Koalendar.",
     contactBox: "I agree to the privacy policy and I consent to being contacted by Qizify via email. I agree to the storage of my transmitted data for the required period of time."
   }
 }
