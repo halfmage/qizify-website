4e1af642b868ab464a97087eaf224e86b8ee9414
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2022-09-25 14:18:08 +0200

    content and smaller fixes

--- src/_data/testimonials.js
+++ src/_data/testimonials.js
@@ -4,16 +4,6 @@ module.exports = {
             image: "testimonial-01.jpg",
             quote: "Ich mag Qizify, weil es mir ermöglicht, mein Wissen in einer spielähnlichen Umgebung zu testen – es ist sicher, Fehler zu machen, und ich fühle mich zum Lernen motiviert.",
             name: "Pavel Raschetnov, student of RWTH Aachen",
-        },
-        {
-            image: "testimonial-02.jpg",
-            quote: "Endlich weiß ich, was ich lernen muss, um bei der Prüfung erfolgreich zu sein.",
-            name: "NAME_TEXT",
-        },
-        {
-            image: "testimonial-03.jpg",
-            quote: "Qizify Learning verbindet mich mit meinen Kommilitonen und bringt uns unserem Lernziel näher.",
-            name: "NAME_TEXT",
         }
     ],
     en: [
@@ -21,16 +11,6 @@ module.exports = {
             image: "testimonial-01.jpg",
             quote: "I like Qizify because it allows me to test my knowledge in a game-like environment - it is safe to make mistakes, and I feel motivated to study.",
             name: "Pavel Raschetnov, student of RWTH Aachen",
-        },
-        {
-            image: "testimonial-02.jpg",
-            quote: "Endlich weiß ich, was ich lernen muss, um bei der Prüfung erfolgreich zu sein.",
-            name: "NAME_TEXT",
-        },
-        {
-            image: "testimonial-03.jpg",
-            quote: "Qizify Learning verbindet mich mit meinen Kommilitonen und bringt uns unserem Lernziel näher.",
-            name: "NAME_TEXT",
         }
     ]
 };
