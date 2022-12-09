1d83fc32b6f5ae7957f94ac537f4839547f807a9
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2022-12-09 14:15:41 +0100

    fixed contentForTeachers german text

--- src/_data/contentForTeachers.js
+++ src/_data/contentForTeachers.js
@@ -1,14 +1,13 @@
 module.exports = {
   de: {
     title: "Als Lehrbeauftragter können Sie Lerninhalte einfach kontrollieren und verbessern",
-    listitem1: "Erstellen Sie Quizze basierend auf ihren Lerninhalten und teilen Sie sie mit Gruppenkollegen.",
-    listitem2: "Erhalten Sie Karma-Punkte, wenn ihr Quiz von einem Dozenten bestätigt oder von anderen Lernenden gemocht wird, und nehmen Sie an der Rangliste teil.",
-    listitem3: "Spielen Sie Quizzes, die von anderen Lernenden erstellt wurden.",
-    listitem4: "Überprüfung, Analyse und Verbesserung der eigenen Lernleistung.",
+    listitem1: "Quiz erstellen, prüfen und verifizieren.",
+    listitem2: "Teilen Sie Feedback mit einem Quizautor, um es zu verbessern.",
+    listitem3: "Überprüfen Sie die Leistung der Lernenden pro Quiz.",
+    listitem4: "Navigieren Sie Lernende zu geeignetem Lernmaterial."
   },
   en: {
     title: "As a lecturer, you can check and improve learning content.",
-    text: "",
     listitem1: "Create, review and verify quizzes.",
     listitem2: "Share feedback with a quiz author to improve it.",
     listitem3: "Check learners' performance per quiz",
