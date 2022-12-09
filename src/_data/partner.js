92d254e827db97822d5a685f38c1e6545c5cbceb
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2022-12-09 14:44:57 +0100

    new partner section

--- src/_data/partner.js
+++ src/_data/partner.js
@@ -1,10 +1,10 @@
 module.exports = {
     de: {
-        title: "Unser Partner",
+        title: "Partner & Unterstützer",
         text: "Die Fachhochschule Dortmund – University of Applied Sciences and Arts ist unser Berater seit der Idee von Qizify Learning. Wir freuen uns, Studierende und Lehrende der FH Dortmund mit unserer Plattform zu unterstützen und ihre Digitalisierung des Lernens auf das nächste Level zu bringen.",
     },
     en: {
-        title: "Our partner",
+        title: "Partners & supporters",
         text: "Fachhochschule Dortmund – University of Applied Sciences and Arts has been our advisor since ideation of Qizify Learning. We are happy to support students and lecturers of FH Dortmund with our platform and bring their digitalization of learning to the next level.",
     }
 };
