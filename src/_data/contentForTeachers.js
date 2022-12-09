aed264c07dee43b8001be8c87dd7f7622edf8a86
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2022-12-09 18:44:05 +0100

    content and typo fixes

--- src/_data/contentForTeachers.js
+++ src/_data/contentForTeachers.js
@@ -1,15 +1,15 @@
 module.exports = {
   de: {
-    title: "Als Lehrbeauftragter können Sie Lerninhalte einfach kontrollieren und verbessern",
-    listitem1: "Quiz erstellen, prüfen und verifizieren.",
-    listitem2: "Teilen Sie Feedback mit einem Quizautor, um es zu verbessern.",
-    listitem3: "Überprüfen Sie die Leistung der Lernenden pro Quiz.",
-    listitem4: "Navigieren Sie Lernende zu geeignetem Lernmaterial."
+    title: "Als Lehrbeauftragter können Sie Lerninhalte einfach kontrollieren und verbessern.",
+    listitem1: "Quizze erstellen, prüfen und verifizieren.",
+    listitem2: "Quizautoren Feedback geben, um Kenntnisse und Inhalte zu verbessern.",
+    listitem3: "Die Leistung der Lernenden pro Quiz überprüfen.",
+    listitem4: "Lernende zu geeigneten Lernmaterialien navigieren."
   },
   en: {
     title: "As a lecturer, you can check and improve learning content.",
     listitem1: "Create, review and verify quizzes.",
-    listitem2: "Share feedback with a quiz author to improve it.",
+    listitem2: "Share feedback with a quiz author to improve knowledge and content quality.",
     listitem3: "Check learners' performance per quiz",
     listitem4: "Navigate learners to suitable study material."
   }
