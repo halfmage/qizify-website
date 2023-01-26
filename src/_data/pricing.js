4d05500e0198f22f562627df7788df971d7bb347
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2023-01-26 12:25:54 +0100

    pricing page update

--- src/_data/pricing.js
+++ src/_data/pricing.js
@@ -1,34 +1,34 @@
 module.exports = {
     de: {
-        title: "Wir glauben an ein simples Bezahlsystem",
+        title: "Ein kalkulierbares, einfaches Bezahlsystem",
         text: "Der Zugang zu Bildung und Lernmethoden sollte so einfach wie möglich gestaltet werden. Daher haben wir uns bei Qizify entschieden, ein einfaches, auf benötigten Umfang, Preismodell zu verwenden. Dadurch können, neben der vereinfachten Kalkulation von Kosten, ebenfalls Transparenz und Änderungswünsche gewährleistet werden.",
         textListItem1: "Alle Features für eine eigenständige Universitäts-Lernplattform.",
         textListItem2: "100% DSGVO-konform und durch SSL-Verbindungen abgesichert.",
         textListItem3: "Überprüfung, Analyse und Verbesserung der Lernleistung Ihrer Lernenden.",
-        perUser: "pro Nutzer",
+        perUser: "Preis / Nutzer auf Anfrage",
         monthly: "Monatliche Gebühren",
         adjustments: "Anpassungen möglich",
         boxListItem1: "Beinhaltet Dozenten-Onboarding, Software-Updates und Support.",
         boxListItem2: "Die Vertragsdauer beträgt mindestens 6 Monate.",
         boxListItem3: "Keine automatische Vertragsverlängerung.",
         boxListItem4: "Preisnachlass für große Einrichtungen & Live-Demo auf Anfrage.",
-        buttonLabel: "Jetzt kontaktieren"
+        buttonLabel: "Kontakieren Sie uns"
 
     },
     en: {
-        title: "We believe in a simple payment system",
+        title: "A calculable, simple payment system",
         text: "Access to education and learning methods should be made as easy as possible. Therefore, at Qizify, we decided to use a simple, on-demand pricing model. In addition to the simplified calculation of costs, transparency and change requests can also be guaranteed.",
         textListItem1: "All features for an independent university learning platform.",
         textListItem2: "100% GDPR compliant and secured by SSL connections.",
         textListItem3: "Review, analysis, and improvement of learning performance of your learners.",
-        perUser: "per user",
+        perUser: "Price / user on request",
         monthly: "Monthly Fees",
         adjustments: "adjustments possible",
         boxListItem1: "Includes lecturer onboarding, software updates, and support.",
         boxListItem2: "The contract period is at least 6 months.",
         boxListItem3: "No automatic contract extension.",
         boxListItem4: "Price discount for large facilities & live demo on request.",
-        buttonLabel: "Contact now"
+        buttonLabel: "Contact us"
 
     }
 };
