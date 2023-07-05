bb9cfb97ebced0b9eb9e8a9748dff07a4e92be8b
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2023-07-05 22:09:24 +0200

    added ai content

---
+++ src/_data/contentForAi.js
@@ -0,0 +1,20 @@
+module.exports = {
+    de: {
+        title: "Quizze erstellen mit KI",
+        text: "Entdecken Sie unsere innovative Funktion, die Lernenden und Lehrenden ermöglicht, innerhalb ihrer Organisation relevante Bewertungen 15-mal schneller zu erstellen, überprüfen, anzupassen und zu teilen! Verabschieden Sie sich von zeitaufwändiger Prüfungsvorbereitung und der Erstellung obligatorischer Tests, während Sie mühelos relevante Quizinhalte generieren und darauf zugreifen. Erreichen Sie eine neue Ebene der Bildungsexzellenz mit Leichtigkeit.",
+        list1: "KI füllt Fragekarten automatisch für die Überprüfung und Anpassung durch Menschen aus und spart dabei 93% wertvolle Zeit.",
+        list2: "Generieren Sie blitzschnell bis zu 10 Fragen mit Antworten.",
+        list3: "KI erstellt falsche Antwortoptionen zur Begleitung der richtigen Antwort.",
+        list4: "KI bietet erläuternde Kommentare zu richtigen Antworten.",
+        list5: "Verfügbar für Multiple-Choice, offene Fragen und Wissenstest-Quiz!"
+    },
+    en: {
+        title: "Create quizzes with AI",
+        text: "Introducing our groundbreaking feature that empowers learners and educators to create, review, adjust, and share relevant assessments within their organisation at an impressive 15 times the speed! Say goodbye to time-consuming exam preparations and creation of mandatory tests as you leverage this tool to swiftly generate and access relevant quiz content. Embrace a future where educational mastery is within your reach like never before.",
+        list1: "AI autofills question cards for human review and adjustments, saving 93% of valuable time.",
+        list2: "Generate up to 10 questions with answer options in a matter of seconds.",
+        list3: "Let the AI generate incorrect answer options to accompany the correct answer.",
+        list4: "AI provides comments to offer further explanations for the correct answers.",
+        list5: "Available for multiple-choice, open-ended, and knowledge level quiz types!"
+    }
+};  
