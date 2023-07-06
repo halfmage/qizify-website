e2a23883a682b61997bcfc48d953d15075181012
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2023-07-06 11:21:17 +0200

    updated content

--- src/_data/contentForAi.js
+++ src/_data/contentForAi.js
@@ -1,9 +1,9 @@
 module.exports = {
     de: {
-        badge: "Jetzt mit KI Quizze erstellen",
+        badge: "Quizze 15-mal schneller mit KI erstellen!",
         learnmore: "Mehr erfahren",
-        title: "Quizze erstellen mit KI",
-        text: "Entdecken Sie unsere innovative Funktion, die Lernenden und Lehrenden ermöglicht, innerhalb ihrer Organisation relevante Bewertungen 15-mal schneller zu erstellen, überprüfen, anzupassen und zu teilen! Verabschieden Sie sich von zeitaufwändiger Prüfungsvorbereitung und der Erstellung obligatorischer Tests, während Sie mühelos relevante Quizinhalte generieren und darauf zugreifen. Erreichen Sie eine neue Ebene der Bildungsexzellenz mit Leichtigkeit.",
+        title: "Quizze 15-mal schneller erstellen!",
+        text: "Entdecken Sie unsere innovative Funktion, die Lernenden und Lehrenden ermöglicht, innerhalb ihrer Organisation relevante Bewertungen 15-mal schneller zu erstellen, überprüfen, anzupassen und zu teilen! Verabschieden Sie sich von zeitaufwändiger Prüfungsvorbereitung und der Erstellung obligatorischer Tests. Erreichen Sie eine neue Ebene der Bildungsexzellenz mit Leichtigkeit.",
         list1: "KI füllt Fragekarten automatisch für die Überprüfung und Anpassung durch Menschen aus und spart dabei 93% wertvolle Zeit.",
         list2: "Generieren Sie blitzschnell bis zu 10 Fragen mit Antworten.",
         list3: "KI erstellt falsche Antwortoptionen zur Begleitung der richtigen Antwort.",
@@ -11,13 +11,13 @@ module.exports = {
         list5: "Verfügbar für Multiple-Choice, offene Fragen und Wissenstest-Quiz!"
     },
     en: {
-        badge: "Now with quiz AI-assistent",
+        badge: "Create quizzes 15x faster with AI!",
         learnmore: "Learn more",
-        title: "Create quizzes with AI",
-        text: "Introducing our groundbreaking feature that empowers learners and educators to create, review, adjust, and share relevant assessments within their organisation at an impressive 15 times the speed! Say goodbye to time-consuming exam preparations and creation of mandatory tests as you leverage this tool to swiftly generate and access relevant quiz content. Embrace a future where educational mastery is within your reach like never before.",
-        list1: "AI autofills question cards for human review and adjustments, saving 93% of valuable time.",
+        title: "Create quizzes 15x faster!",
+        text: "Introducing our groundbreaking feature that empowers learners and educators to create, review, adjust, and share relevant assessments within their organisation at an impressive 15 times the speed! Say goodbye to time-consuming exam preparations and creation of mandatory tests. Embrace a future where educational mastery is within your reach like never before.",
+        list1: "AI autofills quiz cards for human review and adjustments, saving 93% of time.",
         list2: "Generate up to 10 questions with answer options in a matter of seconds.",
-        list3: "Let the AI generate incorrect answer options to accompany the correct answer.",
+        list3: "AI generates incorrect answer options alongside the correct answer.",
         list4: "AI provides comments to offer further explanations for the correct answers.",
         list5: "Available for multiple-choice, open-ended, and knowledge level quiz types!"
     }
