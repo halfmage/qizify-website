e226392129f4398884d8f4daebc1e3dda1865065
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2024-01-25 22:26:01 +0100

    update contact form / pricing page

--- src/_data/pricing.js
+++ src/_data/pricing.js
@@ -1,34 +1,73 @@
 module.exports = {
     de: {
+        payment: "Pro Monat. Jährliche Abrechnung.",
         title: "Ein kalkulierbares, einfaches Bezahlsystem",
-        text: "Der Zugang zu Bildung und Lernmethoden sollte so einfach wie möglich gestaltet werden. Daher haben wir uns bei Qizify entschieden, ein einfaches, auf benötigten Umfang, Preismodell zu verwenden. Dadurch können, neben der vereinfachten Kalkulation von Kosten, ebenfalls Transparenz und Änderungswünsche gewährleistet werden.",
-        textListItem1: "Alle Features für Ihre eigenständige Lernplattform.",
-        textListItem2: "100% DSGVO-konform und durch SSL-Verbindungen abgesichert.",
-        textListItem3: "Überprüfung, Analyse und Verbesserung der Lernleistung Ihrer Lernenden.",
-        perUser: "Preis / Nutzer auf Anfrage",
-        monthly: "Monatliche Gebühren",
-        adjustments: "Anpassungen möglich",
-        boxListItem1: "Beinhaltet Lehrkräfte- und Mentoren-Onboarding, Software-Updates und Support.",
-        boxListItem2: "30-tägige kostenlose Testversion. Vertragsdauer mindestens 12 Monate.",
-        boxListItem3: "Keine automatische Vertragsverlängerung.",
-        boxListItem4: "Preisnachlass für große Einrichtungen & Live-Demo auf Anfrage.",
-        buttonLabel: "Kontakieren Sie uns"
+
+        standardTitle: "Standard mit",
+        standardText: "Bring deine gamifizierte Erfahrung auf ein neues Level! Organisiere 15-mal schneller mit der Magie von LearnAI.",
+        standardList: [
+            "Private platform with customized learning modules (White-label)",
+            "Question generator based on modules and topics (AI-assisted)",
+            "Detailed dashboards for tracking engagement",
+            "5 hours of personalized consulting",
+            "All data stored securely in Germany"         
+        ],
+
+        premiumTitle: "Premium mit",
+        premiumText: "Verwandle Dokumente sofort in personalisierte Inhalte! Verbessere deine Schulungserfahrung mit LearnAI+",
+        premiumList: [
+            "Alles aus Standard",
+            "PDF-to-Quiz-Generator (unterstützt durch KI)",
+            "Text-to-Quiz-Generator (unterstützt durch KI)",
+            "Maßgeschneiderter Quizinhalt für deine Organisation",
+            "Premium-Bildbibliothek",
+            "Bis 7 Stunden persönliche Beratung",
+        ],
+
+        ultimateTitle: "Ultimate mit",
+        ultimateText: "Über Inhalte hinaus, gestalte Lernreise und Softwarefunktionen individuell. Verbessere Bildung, einzigartig für Ihr Unternehmen!",
+        ultimateList: [
+            "Alles aus Premium",
+            "Single-Sign-On-Integration",
+            "Premium-maßgeschneidertes Design",
+            "Umfassende unternehmensspezifische Funktionen",
+            "Unbegrenzte Beratung während der Geschäftszeiten",
+        ],
 
     },
     en: {
+        payment: "Per month. Billed annually.",
         title: "A calculable, simple payment system",
-        text: "Access to education and learning methods should be made as easy as possible. Therefore, at Qizify, we decided to use a simple, on-demand pricing model. In addition to the simplified calculation of costs, transparency and change requests can also be guaranteed.",
-        textListItem1: "All features for your dedicated learning platform.",
-        textListItem2: "100% GDPR compliant and secured by SSL connections.",
-        textListItem3: "Review, analysis, and improvement of performance of your learners.",
-        perUser: "Price / user on request",
-        monthly: "Monthly Fees",
-        adjustments: "adjustments possible",
-        boxListItem1: "Includes onboarding of lecturers and mentors, software updates, and support.",
-        boxListItem2: "30-days free trial. Contract period is at least 12 months.",
-        boxListItem3: "No automatic contract extension.",
-        boxListItem4: "Price discount for large facilities & live demo on request.",
-        buttonLabel: "Contact us"
 
+        standardTitle: "Standard with",
+        standardText: "Elevate Your Gamified Experience! Organize 15x faster with the magic of LearnAI.",
+        standardList: [
+            "Private platform with customized learning modules (White-label)",
+            "Question generator based on modules and topics (AI-assisted)",
+            "Detailed dashboards for tracking engagement",
+            "5 hours of personalized consulting",
+            "All data stored securely in Germany"         
+        ],
+
+        premiumTitle: "Premium with",
+        premiumText: "Transform organizational docs into personalized content instantly! Elevate your training experience with LearnAI+.",
+        premiumList: [
+            "Everything in Standard",
+            "PDF-to-Quiz generator (AI-assisted)",
+            "Text-to-Quiz generator (AI-assisted)",
+            "Tailored quiz content for your organization",
+            "Premium Image library",
+            "7 hours of personalized consultation",
+        ],
+
+        ultimateTitle: "Ultimate mit",
+        ultimateText: "Beyond content, customize your learning journey and software features. Elevate education, uniquely yours!",
+        ultimateList: [
+            "Everything in Premium",
+            "Single-Sign-On Integration",
+            "Premium Customized Design",
+            "Extensive Company-Specific Functionalities",
+            "Unlimited Consulting During Business Hours",
+        ],
     }
 };
