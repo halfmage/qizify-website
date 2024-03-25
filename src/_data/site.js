6d1cc9c112f2c57be7d84a9eaf6e13f566768cbc
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2024-03-25 17:23:38 +0100

    Added banner partial

--- src/_data/site.js
+++ src/_data/site.js
@@ -38,7 +38,8 @@ module.exports = {
       "Steigern Sie mit interaktiven Lernspielen die Motivation der Teilnehmenden. Inspirieren Sie durch Edutainment = Education & Entertainment, und halten Sie den Energielevel hoch.",
       "Sparen Sie sich Ihre wertvolle Zeit durch interaktive Methoden, die sofort in Ihrer Lehre einsetzbar sind.",
       "Sonderangebot: Hol dir jetzt einen exklusiven Rabatt von 8 % mit dem Code “Edutainerin-08”! Gilt nicht für Bestandskunden von Die Edutainerin"
-    ]
+    ],
+    NewName: "Qizify Learning heißt nun LearnSlice!"
   },
   en: {
     title: "LearnSlice",
@@ -78,6 +79,7 @@ module.exports = {
       "Increase the motivation of the participants with interactive learning games. Inspire through Edutainment = Education & Entertainment, and keep the energy level high.",
       "Save your valuable time with interactive methods that can be used immediately in your teaching.",
       "Special offer: Get an exclusive discount of 8 % with the code “Edutainerin-08”! Not valid for existing customers of Die Edutainerin."
-    ]
+    ],
+    NewName: "Qizify Learning is now LearnSlice!"
   }
 }
