3e30125dce50fd1c6610ff3c8952cf3b231c44cf
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2024-03-25 17:26:34 +0100

    Update favicon

--- .eleventy.js
+++ .eleventy.js
@@ -28,12 +28,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/robots.txt')
   
 	// SEO and Opengraph stuff
-  eleventyConfig.addPassthroughCopy('src/assets/images/favicon.ico')
-  eleventyConfig.addPassthroughCopy('src/assets/images/favicon-16x16.png')
-  eleventyConfig.addPassthroughCopy('src/assets/images/favicon-32x32.png')
-  eleventyConfig.addPassthroughCopy('src/assets/images/android-chrome-192x192.png')
-  eleventyConfig.addPassthroughCopy('src/assets/images/android-chrome-512x512.png')
-  eleventyConfig.addPassthroughCopy('src/assets/images/site.webmanifest')
+  eleventyConfig.addPassthroughCopy('src/favicon.svg')
 
   eleventyConfig.addShortcode('version', function () {
     return now
