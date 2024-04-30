af64c781a9f7f6679859265d8bfa32870d5c00b3
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2024-04-30 13:00:55 +0200

    Update partner sections

--- src/_data/partner.js
+++ src/_data/partner.js
@@ -1,10 +1,12 @@
 module.exports = {
     de: {
-        title: "Unsere Partner & Unterstützer",
+        title: "Unsere prominenten Kunden",
+        footerTitle: "Unsere Unterstützer",
         text: "Die Fachhochschule Dortmund – University of Applied Sciences and Arts ist unser Berater seit der Idee von LearnSlice. Wir freuen uns, Studierende und Lehrende der FH Dortmund mit unserer Plattform zu unterstützen und ihre Digitalisierung des Lernens auf das nächste Level zu bringen.",
     },
     en: {
-        title: "Our partners & supporters",
+        title: "Our featured clients",
+        footerTitle: "Our supporters",
         text: "Fachhochschule Dortmund – University of Applied Sciences and Arts has been our advisor since ideation of LearnSlice. We are happy to support students and lecturers of FH Dortmund with our platform and bring their digitalization of learning to the next level.",
     }
 };
