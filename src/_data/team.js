cdcc1d8d256e0283772b0fbd0a620884977afc7a
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2025-03-05 15:39:13 +0100

    Update legal documents

--- src/_data/team.js
+++ src/_data/team.js
@@ -2,7 +2,7 @@ module.exports = {
     de: [
         {
             image: "alesia",
-            name: "Alesia Kunts",
+            name: "Alesia Kunz",
             position: "CEO",
             description: "Neugierigere Ideengeberin",
             text: "Ich liebe es zu lernen. Ich glaube, ich habe nie aufgehört zu lernen, weil ich ohne es nicht leben kann. Wir haben uns entschieden, den Lernprozess digital, effizient und unterhaltsam zu gestalten. Daher haben wir unsere Fähigkeiten genutzt, um etwas Großartiges zu schaffen und es mit anderen zu teilen - LearnSlice."
@@ -32,7 +32,7 @@ module.exports = {
     en: [
         {
             image: "alesia",
-            name: "Alesia Kunts",
+            name: "Alesia Kunz",
             position: "CEO",
             description: "Inquisitive ideator",
             text: "I love learning. I believe I never stopped learning as I cannot live without it. We decided to make the learning process digital, efficient, and entertaining. Thus, we used our skills to create something great and share it with others - LearnSlice."
